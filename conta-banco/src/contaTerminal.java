18bf9466ea67a8409dcc8a844be7b9b60dd75ab3
Bruna Carvalho <brucarv.nah@gmail.com>
2023-10-21 21:04:37 -0300

    Finalizando o mini desafio "Conta-banco"

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner leitor = new Scanner(System.in);
+        
+       System.out.println("Por favor, digite o número da agência: ");
+       int numero = leitor.nextInt();
+
+       leitor.nextLine();
+
+       System.out.println("Agora, digite o nome da agência: ");
+        String agencia = leitor.nextLine();
+
+       System.out.println("Nome de cliente: ");
+        String nomeCliente = leitor.nextLine();
+        
+       System.out.println("Insira o valor do saldo Atual: ");
+        double saldo = leitor.nextDouble();
+
+        leitor.nextLine();
+
+       System.out.println("Olá " + nomeCliente + " Obrigado por criar uma conta em nosso banco, sua agência é " + 
+        agencia + ", conta " + numero + " e seu Saldo " + saldo + " já está disponível para saque.");
+
+        leitor.close();
     }
 }
